6b0325131a217f309d94c3c1e18d874e0dd1a458
pindaroso <me@pindaroso.com>
2024-10-09 16:43:39 -0400

    Added shadcn for UI

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css')
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
---
+++ packages/app/src/components/send-button.tsx
@@ -0,0 +1,138 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { FC, useCallback } from 'react'
+import {
+  LightSystemProgram,
+  Rpc,
+  bn,
+  buildTx,
+  confirmTx,
+  createRpc,
+  defaultTestStateTreeAccounts,
+  selectMinCompressedSolAccountsForTransfer,
+} from '@lightprotocol/stateless.js'
+import {
+  ComputeBudgetProgram,
+  Keypair,
+  TransactionMessage,
+  VersionedTransaction,
+} from '@solana/web3.js'
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base'
+
+import { Button } from '@/components/ui/button'
+
+export const SendButton: FC = () => {
+  const { publicKey, sendTransaction } = useWallet()
+
+  const onClick = useCallback(async () => {
+    /// Get connection with compatibility to Compression API
+    const connection: Rpc = createRpc()
+
+    if (!publicKey) throw new WalletNotConnectedError()
+
+    /// airdrop
+    await confirmTx(connection, await connection.requestAirdrop(publicKey, 1e9))
+
+    /// compress to self
+    const compressInstruction = await LightSystemProgram.compress({
+      payer: publicKey,
+      toAddress: publicKey,
+      lamports: 1e8,
+      outputStateTree: defaultTestStateTreeAccounts().merkleTree,
+    })
+
+    const compressInstructions = [
+      ComputeBudgetProgram.setComputeUnitLimit({ units: 1_000_000 }),
+      compressInstruction,
+    ]
+
+    const {
+      context: { slot: minContextSlot },
+      value: blockhashCtx,
+    } = await connection.getLatestBlockhashAndContext()
+
+    const tx = buildTx(compressInstructions, publicKey, blockhashCtx.blockhash)
+
+    const signature = await sendTransaction(tx, connection, {
+      minContextSlot,
+    })
+
+    await connection.confirmTransaction({
+      blockhash: blockhashCtx.blockhash,
+      lastValidBlockHeight: blockhashCtx.lastValidBlockHeight,
+      signature,
+    })
+
+    console.log(
+      `Compressed ${1e8} lamports! txId: https://explorer.solana.com/tx/${signature}?cluster=custom`
+    )
+
+    /// Send compressed SOL to a random address
+    const recipient = Keypair.generate().publicKey
+
+    /// 1. We need to fetch our sol balance
+    const accounts = await connection.getCompressedAccountsByOwner(publicKey)
+
+    console.log('accounts', accounts.items)
+    const [selectedAccounts, _] = selectMinCompressedSolAccountsForTransfer(
+      accounts.items,
+      1e7
+    )
+
+    console.log('selectedAccounts', selectedAccounts)
+    /// 2. Retrieve validity proof for our selected balance
+    const { compressedProof, rootIndices } = await connection.getValidityProof(
+      selectedAccounts.map((account) => bn(account.hash))
+    )
+
+    /// 3. Create and send compressed transfer
+    const sendInstruction = await LightSystemProgram.transfer({
+      payer: publicKey,
+      toAddress: recipient,
+      lamports: 1e7,
+      inputCompressedAccounts: selectedAccounts,
+      outputStateTrees: [defaultTestStateTreeAccounts().merkleTree],
+      recentValidityProof: compressedProof,
+      recentInputStateRootIndices: rootIndices,
+    })
+    const sendInstructions = [
+      ComputeBudgetProgram.setComputeUnitLimit({ units: 1_000_000 }),
+      sendInstruction,
+    ]
+
+    const {
+      context: { slot: minContextSlotSend },
+      value: {
+        blockhash: blockhashSend,
+        lastValidBlockHeight: lastValidBlockHeightSend,
+      },
+    } = await connection.getLatestBlockhashAndContext()
+
+    const messageV0Send = new TransactionMessage({
+      payerKey: publicKey,
+      recentBlockhash: blockhashSend,
+      instructions: sendInstructions,
+    }).compileToV0Message()
+
+    const transactionSend = new VersionedTransaction(messageV0Send)
+
+    const signatureSend = await sendTransaction(transactionSend, connection, {
+      minContextSlot: minContextSlotSend,
+    })
+
+    await connection.confirmTransaction({
+      blockhash: blockhashSend,
+      lastValidBlockHeight: lastValidBlockHeightSend,
+      signature: signatureSend,
+    })
+
+    console.log(
+      `Sent ${1e7} lamports to ${recipient.toBase58()} ! txId: https://explorer.solana.com/tx/${signatureSend}?cluster=custom`
+    )
+  }, [publicKey, sendTransaction])
+
+  return (
+    <Button onClick={onClick} disabled={!publicKey}>
+      Get airdrop, compress and send SOL to a random address!
+    </Button>
+  )
+}
