8d3dd718e8d00f10e86c9ceb7e6b1df103a66478
pindaroso <me@pindaroso.com>
2024-10-13 10:13:56 -0400

    Updated create counter example

--- packages/app/src/components/buttons/create-counter.tsx
+++ packages/app/src/components/buttons/create-counter.tsx
@@ -1,29 +1,15 @@
 import { useAnchorWallet, useWallet } from '@solana/wallet-adapter-react'
 import { FC, useCallback } from 'react'
 import {
-  confirmTx,
   createRpc,
   LightSystemProgram,
-  Rpc,
   bn,
-  buildTx,
   defaultTestStateTreeAccounts,
-  packNewAddressParams,
-  PackedMerkleContext,
-  CompressedAccount,
-  CompressedAccountData,
-  NewAddressParamsPacked,
-  LightCompressedToken,
-  LightRegistry,
-  LightSystem,
-  packCompressedAccounts,
-  deriveAddress,
   noopProgram,
   accountCompressionProgram,
-  createCompressedAccount,
-  compress,
+  deriveAddress,
 } from '@lightprotocol/stateless.js'
-import { createMint } from '@lightprotocol/compressed-token'
+import { keccak_256 } from '@noble/hashes/sha3'
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base'
 import { PublicKey, SystemProgram } from '@solana/web3.js'
 import toast from 'react-hot-toast'
@@ -32,29 +18,65 @@ import { useWalletContext } from '@/components/providers/wallet'
 import { useProgramContext } from '@/components/providers/program'
 import { Button } from '@/components/ui/button'
 
+export function hashvToBn254FieldSizeBe(bytes: Uint8Array[]): Uint8Array {
+  const hasher = keccak_256.create()
+  for (const input of bytes) {
+    hasher.update(input)
+  }
+  const hash = hasher.digest()
+  hash[0] = 0
+  return hash
+}
+
+export function deriveAddressSeed(
+  seeds: Uint8Array[],
+  programId: PublicKey
+): Uint8Array {
+  const combinedSeeds: Uint8Array[] = [programId.toBytes(), ...seeds]
+  const hash = hashvToBn254FieldSizeBe(combinedSeeds)
+  return hash
+}
+
+export class ProgramNotInitializedError extends Error {
+  name = 'ProgramNotInitializedError'
+}
+
 export const CreateButton: FC = () => {
-  const { publicKey } = useWallet()
   const { endpoint } = useWalletContext()
   const { program } = useProgramContext()
   const wallet = useAnchorWallet()
 
   const onClick = useCallback(async () => {
-    if (!publicKey) throw new WalletNotConnectedError()
     if (!wallet) throw new WalletNotConnectedError()
+    if (!program) throw new ProgramNotInitializedError()
 
     try {
       // TODO: Adapt for devnet, testnet and mainnet
       const rpc = createRpc()
 
-      const treeAccounts = defaultTestStateTreeAccounts()
+      const { addressTree, addressQueue } = defaultTestStateTreeAccounts()
 
-      // TODO: Create compressed counteraccoun?
+      const seed = deriveAddressSeed(
+        [Buffer.from('counter'), wallet.publicKey.toBuffer()],
+        program.programId
+      )
+      const address = await deriveAddress(seed, addressTree)
+
+      const selectedAccounts: any[] = [
+        {
+          hash: bn(address.toBytes()),
+          tree: addressTree,
+          queue: addressQueue,
+        },
+      ]
 
       // TODO: Add hashes and new addresses
-      const proofWithContext = await rpc.getValidityProofAndRpcContext()
+      const { compressedProof, rootIndices } = await rpc.getValidityProof(
+        selectedAccounts.map((account: any) => bn(account.hash))
+      )
 
+      // TODO: Add input?
       const inputs: Buffer[] = []
-      const proof = proofWithContext.value.compressedProof
 
       // TODO: Use LUT to get indices?
       const merkleContext = {
@@ -68,7 +90,7 @@ export const CreateButton: FC = () => {
         addressQueuePubkeyIndex: 0,
       }
       const merkleTreeRootIndex = 0
-      const addressMerkleTreeRootIndex = proofWithContext.value.rootIndices[0]
+      const addressMerkleTreeRootIndex = rootIndices[0]
 
       const [registeredProgramPda] = PublicKey.findProgramAddressSync(
         [LightSystemProgram.programId.toBuffer()],
@@ -83,21 +105,21 @@ export const CreateButton: FC = () => {
       const tx = await program.methods
         .create(
           inputs,
-          proof,
+          compressedProof,
           merkleContext,
           merkleTreeRootIndex,
           addressMerkleContext,
           addressMerkleTreeRootIndex
         )
         .accounts({
-          signer: publicKey,
+          signer: wallet.publicKey,
           lightSystemProgram: LightSystemProgram.programId,
           accountCompressionProgram,
           accountCompressionAuthority,
           registeredProgramPda,
           noopProgram,
           selfProgram: program.programId,
-          cpiSigner: publicKey,
+          cpiSigner: wallet.publicKey,
           systemProgram: SystemProgram.programId,
         })
         .rpc()
@@ -107,10 +129,10 @@ export const CreateButton: FC = () => {
       console.error(error)
       toast.error(`Creation Failed: ${error.message}`)
     }
-  }, [publicKey, endpoint])
+  }, [wallet, endpoint, program])
 
   return (
-    <Button onClick={onClick} disabled={!publicKey}>
+    <Button onClick={onClick} disabled={!wallet}>
       Create Counter
     </Button>
   )
